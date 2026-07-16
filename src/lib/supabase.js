e30e8637ee8c25a5924e0f3f84090fbfe377cab1
denisas24 <denisaschonhofer@gmail.com>
2026-07-16 09:29:05 +0200

    fix supabase key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://jxbviayjptjrlymzjhzt.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_U_bo3K-AKKNkVQ6-KHZXaw_tydwAWOE';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4YnZpYXlqcHRqcmx5bXpqaHp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxMzg1NTAsImV4cCI6MjA5OTcxNDU1MH0.M4N5loMPg_owi3FOwcMNMIRDdo7a_9TatqXlAk227KM';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
